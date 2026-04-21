b004b3b5d720d18d157ceda4935fd85f3d527977
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-04-21 13:51:38 +0530

    delete some file

--- aryan-route-loader/example/routes/posts.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/posts", (req, res) => {
-  res.json({ message: "Posts route working!" });
-});
-
-module.exports = router;
